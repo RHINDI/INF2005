edca3626783bc33f35358e2903d4216e314b378b
Rhindi Youssef <rhindi.youssef@courrier.uqam.ca>
2015-12-14 12:47:57 -0500

    change the video in iframe on click directeur menu

--- js/main.js
+++ js/main.js
@@ -2,4 +2,22 @@
  * Created by rhindi on 12/12/15.
  */
 
-// When any a's in header nav is hovered
+// Change the nav On hover
+$("nav a").hover(
+    function(){
+    var urlicon = $(this).css('background').replace('white', 'hover');
+        $(this).css('background', urlicon)
+        $(this).addClass('hover');
+    }, function(){
+        var urlicon = $(this).css('background').replace('hover', 'white');
+        $(this).css('background', urlicon)
+        $(this).removeClass("hover");
+    });
+
+// Change the video in the iframe
+$(".directeur").click(
+    function(){
+        $("#1").attr("src","https://www.youtube.com/embed/Myq5V3ldiMA");
+    });
+
+
