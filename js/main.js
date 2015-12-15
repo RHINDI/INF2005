ae0646844f8b052b5f38812e333a02a1c6c80951
Rhindi Youssef <rhindi.youssef@courrier.uqam.ca>
2015-12-15 09:45:21 -0500

    add logIn.html

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ $(".directeur").click(
         $("#1").attr("src","https://www.youtube.com/embed/Myq5V3ldiMA");
     });
 
-// Popup window code
+// Popup productDetails.html
 $('#product a').click(
     function () {
         var url ="./html/productDetails.html";
@@ -37,3 +37,21 @@ $('#product a').click(
             'location=no,' + 'directories=no,'+
             'status=no')
 });
+
+// Popup logIn.html
+$('#product a').click(
+    function () {
+        var url ="./html/productDetails.html";
+        var pWidth = 1160;
+        var pHeight= 745;
+        var center_left = (screen.width / 2) - (pWidth / 2);
+        var center_top = (screen.height / 2) - (pHeight / 2);
+        window.open(
+            url,'popUpWindow',
+            'height='+ pHeight + ',width=' + pWidth +
+            ',left=' + center_left + ',top=' + center_top +
+            ',resizable=no,' + 'scrollbars=no,' +
+            'toolbar=no,' + 'menubar=no,' +
+            'location=no,' + 'directories=no,'+
+            'status=no')
+    });
